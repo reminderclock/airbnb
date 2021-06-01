5fc0531bade625c8851f5c6c0a4a371bce868ac9
reminderclock <reminderclock@naver.com>
2021-06-01 16:59:56 +0900

    feat: Get  Price static fetch data(#11)

--- frontend/front-airbnb/src/components/modal/PriceModal.jsx
+++ frontend/front-airbnb/src/components/modal/PriceModal.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styled from 'styled-components';
 import {Slider} from "@material-ui/core";
+import axios from 'axios';
 
 const PriceModal = () => {
     const gfg = [
@@ -17,11 +18,31 @@ const PriceModal = () => {
       const updateRange = (e, data) => {
         setVal(data);
       };
+      const [range, setRange] = useState(null);
+      const API_URL ='/rooms/charges?check-in="2021-05-10"&check-out="2021-05-20"'
+      useEffect(() => {
+        loadData();
+      },[]);
+      const loadData = () => {
+        axios.get(API_URL).then((response) => {
+          setValue(response.data.charges);
+          setAverage(response.data.average)
+        })
+      }
+      const [average, SetAverage] = useState(0);
+      const setAverage = (average) => {
+        SetAverage(average);
+      }
+      const setValue = (chargeData) => {
+        setRange(chargeData);
+        setVal([chargeData[0].charge, chargeData[chargeData.length-1].charge]);
+      }
     return (
         <PriceModalWrapper className="PriceModal" onClick={e => e.stopPropagation()}>
             <PriceContainer>
-                <span> min/max : </span>
+                <span> 가격범위 </span>
                 {val.map(e => <div>{e}</div>)}
+                <div>{`평균 1박 요금은 ₩${average}입니다.`}</div>
                 <Slider value={val} onChange={updateRange} marks={gfg} />
             </PriceContainer>{" "}
         </PriceModalWrapper>
